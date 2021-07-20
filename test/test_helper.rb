5b06730294a8eda3df03b10fa7f7eec907c8f307
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-20 11:16:19 +0100

    Try again to please Rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,8 @@ $:.unshift File.expand_path("../lib", __dir__)
 require "regular_expression"
 require "graphviz"
 unless `which dot`.chomp.end_with?("dot")
-  $stderr.puts "YOU HAVE NOT INSTALLED GRAPHVIZ. We found no 'dot' in your path.\n" +
-    "Please install Graphviz if you want dotfile visual output to work."
+  warn "YOU HAVE NOT INSTALLED GRAPHVIZ. We found no 'dot' in your path.\n" \
+       "Please install Graphviz if you want dotfile visual output to work."
 end
 
 require "minitest/autorun"
