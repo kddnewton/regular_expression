6aa1f117c3cc2ab7548bbb4b25d1a003029fe441
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-20 11:09:16 +0100

    Mention graphviz in the README, both installation and dev sections. Check for graphviz when loading the parser executable or running tests (same places we require graphviz.)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,8 @@ SimpleCov.start
 $:.unshift File.expand_path("../lib", __dir__)
 require "regular_expression"
 require "graphviz"
+unless `which dot`.chomp.end_with?("dot")
+  STDERR.puts "YOU HAVE NOT INSTALLED GRAPHVIZ. We found no 'dot' in your path. Please install Graphviz if you want dotfile visual output to work."
+end
 
 require "minitest/autorun"
