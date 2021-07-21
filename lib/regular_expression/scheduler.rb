805c4ff25cb887450022aa9dd89d59dd051c39a4
Chris Seaton <chris.seaton@shopify.com>
2021-07-21 17:13:50 +0100

    Full scheduler fallback for any errors

--- lib/regular_expression/scheduler.rb
+++ lib/regular_expression/scheduler.rb
@@ -33,78 +33,86 @@ module RegularExpression
 
       deferred = []
 
-      until schedule.size == cfg.blocks.values.size
-        # The last scheduled block.
-        just_scheduled = schedule.last
+      begin
+        until schedule.size == cfg.blocks.values.size
+          # The last scheduled block.
+          just_scheduled = schedule.last
 
-        # Successors of the last scheduled block that still need to be
-        # scheduled.
-        succs_to_schedule = just_scheduled.exits.reject { |e| schedule.include?(cfg.blocks[e.label]) }
+          # Successors of the last scheduled block that still need to be
+          # scheduled.
+          succs_to_schedule = just_scheduled.exits.reject { |e| schedule.include?(cfg.blocks[e.label]) }
 
-        # Successors of the last scheduled block that are ready to be scheduled.
-        sucss_ready = succs_to_schedule.select { |e| ready?(schedule, cfg.blocks[e.label]) }
+          # Successors of the last scheduled block that are ready to be scheduled.
+          sucss_ready = succs_to_schedule.select { |e| ready?(schedule, cfg.blocks[e.label]) }
 
-        # Are any successors of the last block that was scheduled themselves
-        # ready to be scheduled?
+          # Are any successors of the last block that was scheduled themselves
+          # ready to be scheduled?
 
-        if sucss_ready.any?
-          # Yes - the last block has successors ready to be scheduled.
+          if sucss_ready.any?
+            # Yes - the last block has successors ready to be scheduled.
 
-          # Find the most probbale successor that is ready to be scheduled
-          # (rule 1).
-          most_probable_ready_succ = sucss_ready.max_by { |e| e.metadata[:probability] || 0.0 }
-          most_probable_ready_succ_block = cfg.blocks[most_probable_ready_succ.label]
-          raise if schedule.include?(most_probable_ready_succ_block)
+            # Find the most probbale successor that is ready to be scheduled
+            # (rule 1).
+            most_probable_ready_succ = sucss_ready.max_by { |e| e.metadata[:probability] || 0.0 }
+            most_probable_ready_succ_block = cfg.blocks[most_probable_ready_succ.label]
+            raise if schedule.include?(most_probable_ready_succ_block)
 
-          # Schedule it
-          schedule.push most_probable_ready_succ_block
+            # Schedule it
+            schedule.push most_probable_ready_succ_block
 
-          # If we deferred it for later remove it.
-          deferred.delete most_probable_ready_succ_block
+            # If we deferred it for later remove it.
+            deferred.delete most_probable_ready_succ_block
 
-          # Defer other successors that still need to be scheduled, less the
-          # one that we just scheduled.
-          deferred.push(*((succs_to_schedule - [most_probable_ready_succ]).map { |e| cfg.blocks[e.label] }))
-        elsif deferred.any?
-          # No - none of the successors of the last block are ready to be
-          # scheduled. We do have deferred blocks.
+            # Defer other successors that still need to be scheduled, less the
+            # one that we just scheduled.
+            deferred.push(*((succs_to_schedule - [most_probable_ready_succ]).map { |e| cfg.blocks[e.label] }))
+          elsif deferred.any?
+            # No - none of the successors of the last block are ready to be
+            # scheduled. We do have deferred blocks.
 
-          # Get the earliest deferred block that is ready to be scheduled
-          # (rule 2).
-          first_ready_deferred = deferred.find { |block| ready?(schedule, block) }
+            # Get the earliest deferred block that is ready to be scheduled
+            # (rule 2).
+            first_ready_deferred = deferred.find { |block| ready?(schedule, block) }
 
-          # If we didn't find one ready to be scheduled, just take the first
-          # one (rule 3).
-          first_ready_deferred ||= deferred.first
+            # If we didn't find one ready to be scheduled, just take the first
+            # one (rule 3).
+            first_ready_deferred ||= deferred.first
 
-          deferred.delete first_ready_deferred
-          raise if schedule.include?(first_ready_deferred)
+            deferred.delete first_ready_deferred
+            raise if schedule.include?(first_ready_deferred)
 
-          schedule.push first_ready_deferred
-        elsif (cfg.blocks.values - schedule).all? { |a| a.preds.empty? }
-          # No - none of the successors of the last block are ready to be
-          # scheduled. We don't have deferred blocks. But it's ok because all
-          # remaining blocks are orphans - so we can finish scheduling.
-          break
-        else
-          # The scheduler is broken.
-          blocks = cfg.blocks.values
-          remaining = blocks - schedule
-          ready = remaining.select { |b| ready?(schedule, b) }
-          warn "[warning(regexp)] scheduling failed with #{blocks.size} blocks, " \
-               "#{schedule.size} scheduled, #{remaining.size} remaining, " \
-               "#{ready.size} ready, #{deferred.size} deferred - using fallback scheduler to recover"
-          return blocks
+            schedule.push first_ready_deferred
+          elsif (cfg.blocks.values - schedule).all? { |a| a.preds.empty? }
+            # No - none of the successors of the last block are ready to be
+            # scheduled. We don't have deferred blocks. But it's ok because all
+            # remaining blocks are orphans - so we can finish scheduling.
+            break
+          else
+            # The scheduler is broken.
+            raise
+          end
         end
-      end
 
-      schedule
+        schedule
+      rescue
+        return fallback_schedule(cfg, schedule, deferred)
+      end
     end
 
     # A block that has yet to be scheduled and where all the predecessors
     # except itself have already been scheduled.
     def self.ready?(schedule, block)
       (block.preds - [block]).all? { |pred| schedule.include?(pred) }
+    end
+
+    def self.fallback_schedule(cfg, schedule, deferred)
+      blocks = cfg.blocks.values
+      remaining = blocks - schedule
+      ready = remaining.select { |b| ready?(schedule, b) }
+      warn "[warning(regexp)] scheduling failed with #{blocks.size} blocks, " \
+               "#{schedule.size} scheduled, #{remaining.size} remaining, " \
+               "#{ready.size} ready, #{deferred.size} deferred - using fallback scheduler to recover"
+      blocks
     end
 
     def self.dump(cfg, schedule)
