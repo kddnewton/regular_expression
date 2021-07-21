00bcbfd116f07302dd5b19a14994ec79192cf43f
Chris Seaton <chris.seaton@shopify.com>
2021-07-21 21:48:11 +0100

    Pattern#profile to run a pattern with profile-directed compilation

--- lib/regular_expression/pattern.rb
+++ lib/regular_expression/pattern.rb
@@ -16,6 +16,25 @@ module RegularExpression
       redefine_match(&compiler.compile(cfg, schedule))
     end
 
+    def profile(compiler: Compiler::X86, threshold: 100)
+      interpreter = Interpreter.new(bytecode)
+      profiling_data = Interpreter.empty_profiling_data
+      iteration = threshold
+
+      # Replace with a profiling interpreter version of match?
+      redefine_match do |string|
+        if (iteration -= 1).negative?
+          cfg = CFG.build(bytecode, profiling_data)
+          schedule = Scheduler.schedule(cfg)
+
+          # Replace with a compiled version of match?
+          redefine_match(&compiler.compile(cfg, schedule))
+        end
+
+        interpreter.interpret(string, profiling_data)
+      end
+    end
+
     def match?(string)
       Interpreter.new(bytecode).match?(string)
     end
