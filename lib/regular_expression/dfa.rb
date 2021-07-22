f090fe064cd4fe0a97872ba8575e328d23c23d2c
Tom Stuart <tom.stuart@shopify.com>
2021-07-22 09:38:08 +0100

    Rename next_states to next_nfa_states

--- lib/regular_expression/dfa.rb
+++ lib/regular_expression/dfa.rb
@@ -42,16 +42,16 @@ module RegularExpression
           # the states in our current set to determine where we could end up
           # for any of the transitions.
           transitions.each do |transition|
-            next_states = []
+            next_nfa_states = []
 
             current_nfa_states.flat_map(&:transitions).each do |current_transition|
-              next_states << current_transition.state if transition.matches?(current_transition)
+              next_nfa_states << current_transition.state if transition.matches?(current_transition)
             end
 
             # Now that we have a full set of states that this transition goes
             # to, we're going to create a transition in our DFA that represents
             # this transition.
-            next_nfa_states = next_states_for(next_states)
+            next_nfa_states = next_states_for(next_nfa_states)
 
             unless dfa_states.key?(next_nfa_states)
               # Make sure we check the next states.
