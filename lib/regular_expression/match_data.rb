5108198b1427a241afdc73e311091ce66ac7bc37
Kevin Newton <kddnewton@gmail.com>
2021-07-27 12:57:05 -0400

    Mark named captures as done in the README

--- lib/regular_expression/match_data.rb
+++ lib/regular_expression/match_data.rb
@@ -8,7 +8,21 @@ module RegularExpression
   # Ideally this very closely mirrors that API of the top-level MatchData object
   # so that it's seemless to switch between the two.
   class MatchData
-    attr_reader :string, :indices, :groups, :aliases
+    # This is the original string that was matched against. It can be accessed
+    # through the reader but is also used for pre_match and post_match
+    # calculations.
+    attr_reader :string
+
+    # indices is an array of match indexes. It's a flat array, so to access the
+    # indices for $3 for example, you would access elements 6 and 7.
+    attr_reader :indices
+
+    # groups is a flat array of captures, from $0, $1, $2, and on.
+    attr_reader :groups
+
+    # aliases is a hash of named capture names pointing to an index in the array
+    # groups. It's used for the named_captures method as well as the [] method.
+    attr_reader :aliases
 
     def initialize(string, indices, captures)
       @string = string
@@ -23,12 +37,13 @@ module RegularExpression
       end
     end
 
+    # captures technically doesn't include $0
     def captures
       groups.drop(1)
     end
 
     def named_captures
-      aliases.to_h { |name, index| [name, groups[index]] }
+      aliases.transform_values { |index| groups[index] }
     end
 
     def names
@@ -40,9 +55,11 @@ module RegularExpression
     end
 
     def post_match
-      string[indices[1]..-1]
+      string[indices[1]..]
     end
 
+    # You can use both string and integer keys for [], string will only
+    # correspond to the named captures.
     def [](index)
       if !index.is_a?(String)
         groups[index]
@@ -72,19 +89,21 @@ module RegularExpression
     def ==(other)
       string == other.string && indices == other.indices
     end
-  
+
+    # This method is largely here to mirror the top-level MatchData object. It
+    # should look the same except for the class name.
     def inspect
       indexed_aliases = aliases.invert
       keypairs =
         groups.map.with_index do |group, index|
-          if index == 0
+          if index.zero?
             group.inspect
           else
             "#{indexed_aliases.fetch(index, index)}:#{group.inspect}"
           end
         end
 
-      "#<#{self.class.name} #{keypairs.join(" ")}>"
+      "#<#{self.class.name} #{keypairs.join(' ')}>"
     end
   end
 end
