5a4fabed020b7550a68bbcab58cffba836ece427
Marco Concetto Rudilosso <marco.rudilosso@shopify.com>
2021-07-28 18:54:03 +0100

    more select usage + disasm

--- lib/regular_expression/compiler/cranelift.rb
+++ lib/regular_expression/compiler/cranelift.rb
@@ -4,10 +4,28 @@ module RegularExpression
   module Compiler
     module Cranelift
       class Compiled
-        attr_reader :f_ptr
+        attr_reader :f_ptr, :f_size
 
-        def initialize(f_ptr)
+        def initialize(f_ptr, f_size)
           @f_ptr = f_ptr
+          @f_size = f_size
+        end
+
+        def disasm
+          output = StringIO.new
+
+          crabstone = Crabstone::Disassembler.new(Crabstone::ARCH_X86, Crabstone::MODE_64)
+          ptr = Fiddle::Pointer.new(f_ptr)
+          crabstone.disasm(ptr[0, f_size], f_ptr).each do |insn|
+            output.printf(
+              "0x%<address>x:\t%<instruction>s\t%<details>s\n",
+              address: insn.address,
+              instruction: insn.mnemonic,
+              details: insn.op_str
+            )
+          end
+
+          output.string
         end
 
         def to_proc
@@ -17,6 +35,12 @@ module RegularExpression
             value if value != string.length + 1
           end
         end
+      end
+
+      def self.jump_to_next_block(bcx)
+        block = bcx.create_block
+        bcx.jump(block, [])
+        bcx.switch_to_block(block)
       end
 
       def self.compile(cfg, _schedule)
@@ -96,52 +120,52 @@ module RegularExpression
                 bcx.def_var(string_index, increased)
                 bcx.def_var(flag, flag_val)
               when Bytecode::Insns::TestValue
-                post_length_check_block = bcx.create_block
                 end_block = bcx.create_block
+                
                 string_index_val = bcx.use_var(string_index)
                 bcx.def_var(flag, zero)
                 bcx.br_icmp(:e, string_index_val, string_length, end_block, [])
-                bcx.jump(post_length_check_block, [])
-                bcx.switch_to_block(post_length_check_block)
+
+                jump_to_next_block(bcx)
                 char_ptr = bcx.iadd(string_pointer, string_index_val)
                 char = bcx.load(:I8, char_ptr, 0)
                 expected_char = bcx.iconst(:I8, insn.char.ord)
                 v = bcx.icmp(:e, char, expected_char)
                 flag_val = bcx.select(v, one, zero)
                 increased = bcx.iadd(string_index_val, flag_val)
+                
                 bcx.def_var(string_index, increased)
                 bcx.def_var(flag, flag_val)
                 bcx.jump(end_block, [])
+                
                 bcx.switch_to_block(end_block)
               when Bytecode::Insns::TestValuesInvert
-                no_match_block = bcx.create_block
-                post_length_check_block = bcx.create_block
-                post_not_equals_check_block = bcx.create_block
                 end_block = bcx.create_block
-                string_index_val = bcx.use_var(string_index)
-                bcx.br_icmp(:e, string_index_val, string_length, no_match_block, [])
-                bcx.jump(post_length_check_block, [])
+                after_checks_block = bcx.create_block
 
-                bcx.switch_to_block(post_length_check_block)
+                string_index_val = bcx.use_var(string_index)
+                bcx.def_var(flag, zero)
+                bcx.br_icmp(:e, string_index_val, string_length, end_block, [])
+
+                jump_to_next_block(bcx)
                 char_ptr = bcx.iadd(string_pointer, string_index_val)
                 char = bcx.load(:I8, char_ptr, 0)
+
                 insn.chars.each do |value|
-                  new_block = bcx.create_block
-                  bcx.jump(new_block, [])
-                  bcx.switch_to_block(new_block)
+                  jump_to_next_block(bcx)
                   expected_char = bcx.iconst(:I8, value.ord)
-                  bcx.br_icmp(:e, char, expected_char, no_match_block, [])
+                  v = bcx.icmp(:e, char, expected_char)
+                  flag_val = bcx.select(v, zero, one)
+                  bcx.def_var(flag, flag_val)
+                  bcx.brz(flag_val, after_checks_block, [])
                 end
-                bcx.jump(post_not_equals_check_block, [])
+                bcx.jump(after_checks_block, [])
 
-                bcx.switch_to_block(post_not_equals_check_block)
-                increased = bcx.iadd(string_index_val, one)
+                bcx.switch_to_block(after_checks_block)
+                flag_val = bcx.use_var(flag)
+                increased = bcx.iadd(string_index_val, flag_val)
                 bcx.def_var(string_index, increased)
-                bcx.def_var(flag, one)
-                bcx.jump(end_block, [])
-
-                bcx.switch_to_block(no_match_block)
-                bcx.def_var(flag, zero)
+                bcx.def_var(flag, flag_val)
                 bcx.jump(end_block, [])
 
                 bcx.switch_to_block(end_block)
@@ -180,90 +204,73 @@ module RegularExpression
 
                 bcx.switch_to_block(end_block)
               when Bytecode::Insns::TestType
-                no_match_block = bcx.create_block
-                post_length_check_block = bcx.create_block
-                post_equal_check_block = bcx.create_block
                 end_block = bcx.create_block
-                string_index_val = bcx.use_var(string_index)
-                bcx.br_icmp(:e, string_index_val, string_length, no_match_block, [])
-                bcx.jump(post_length_check_block, [])
 
-                bcx.switch_to_block(post_length_check_block)
+                string_index_val = bcx.use_var(string_index)
+                bcx.def_var(flag, zero)
+                bcx.br_icmp(:e, string_index_val, string_length, end_block, [])
+
+                jump_to_next_block(bcx)
                 char_ptr = bcx.iadd(string_pointer, string_index_val)
                 char = bcx.load(:I8, char_ptr, 0)
                 external_func_addr = bcx.iconst(:I64, insn.type.handle)
                 res = bcx.call_indirect(external_func_sigref, external_func_addr, [char])
                 values = bcx.inst_results(res)
-                bcx.br_icmp(:e, values[0], zero, no_match_block, [])
-                bcx.jump(post_equal_check_block, [])
+                flag_val = bcx.select(values[0], one, zero)
+                increased = bcx.iadd(string_index_val, flag_val)
 
-                bcx.switch_to_block(post_equal_check_block)
-                increased = bcx.iadd(string_index_val, one)
                 bcx.def_var(string_index, increased)
-                bcx.def_var(flag, one)
-                bcx.jump(end_block, [])
-
-                bcx.switch_to_block(no_match_block)
-                bcx.def_var(flag, zero)
+                bcx.def_var(flag, flag_val)
                 bcx.jump(end_block, [])
 
                 bcx.switch_to_block(end_block)
               when Bytecode::Insns::TestPositiveLookahead
-                no_match_block = bcx.create_block
-                post_length_check_block = bcx.create_block
                 end_block = bcx.create_block
-                string_index_val = bcx.use_var(string_index)
-                bcx.br_icmp(:e, string_index_val, string_length, no_match_block, [])
-                bcx.jump(post_length_check_block, [])
 
-                bcx.switch_to_block(post_length_check_block)
+                string_index_val = bcx.use_var(string_index)
+                bcx.def_var(flag, zero)
+                bcx.br_icmp(:e, string_index_val, string_length, end_block, [])
+
+                jump_to_next_block(bcx)
                 char_ptr = bcx.iadd(string_pointer, string_index_val)
 
                 insn.value.each_char.with_index do |c, index|
                   # Move the correct character into the buffer
+                  jump_to_next_block(bcx)
                   lookahead = bcx.iconst(:I64, index)
                   char_ptr_ahead = bcx.iadd(char_ptr, lookahead)
                   char = bcx.load(:I8, char_ptr_ahead, 0)
                   expected_char = bcx.iconst(:I8, c.ord)
-                  bcx.br_icmp(:ne, char, expected_char, no_match_block, [])
-                  next_block = bcx.create_block
-                  bcx.jump(next_block, [])
-                  bcx.switch_to_block(next_block)
+                  v = bcx.icmp(:e, char, expected_char)
+                  flag_val = bcx.select(v, one, zero)
+                  bcx.def_var(flag, flag_val)
+                  bcx.brz(flag_val, end_block, [])
                 end
-
-                bcx.def_var(flag, one)
                 bcx.jump(end_block, [])
-
-                bcx.switch_to_block(no_match_block)
-                bcx.def_var(flag, zero)
-                bcx.jump(end_block, [])
-
                 bcx.switch_to_block(end_block)
               when Bytecode::Insns::TestNegativeLookahead
-                no_match_block = bcx.create_block
-                post_length_check_block = bcx.create_block
                 end_block = bcx.create_block
+
                 string_index_val = bcx.use_var(string_index)
                 bcx.def_var(flag, one)
                 bcx.br_icmp(:e, string_index_val, string_length, end_block, [])
-                bcx.jump(post_length_check_block, [])
 
-                bcx.switch_to_block(post_length_check_block)
+                jump_to_next_block(bcx)
                 char_ptr = bcx.iadd(string_pointer, string_index_val)
+
                 insn.value.each_char.with_index do |c, index|
                   # Move the correct character into the buffer
+                  jump_to_next_block(bcx)
                   lookahead = bcx.iconst(:I64, index)
                   char_ptr_ahead = bcx.iadd(char_ptr, lookahead)
                   char = bcx.load(:I8, char_ptr_ahead, 0)
                   expected_char = bcx.iconst(:I8, c.ord)
-                  bcx.br_icmp(:ne, char, expected_char, end_block, [])
-                  next_block = bcx.create_block
-                  bcx.jump(next_block, [])
-                  bcx.switch_to_block(next_block)
+                  v = bcx.icmp(:ne, char, expected_char)
+                  flag_val = bcx.select(v, one, zero)
+                  bcx.def_var(flag, flag_val)
+                  bcx.brnz(flag_val, end_block, [])
                 end
-                bcx.def_var(flag, zero)
                 bcx.jump(end_block, [])
-
                 bcx.switch_to_block(end_block)
               when Bytecode::Insns::TestRangeInvert
                 no_match_block = bcx.create_block
@@ -337,7 +344,8 @@ module RegularExpression
         })
         b.finalize
         f_ptr = b.get_function_pointer(f)
-        Compiled.new(f_ptr)
+        f_size = b.get_function_size(f)
+        Compiled.new(f_ptr, f_size)
       end
     end
   end
